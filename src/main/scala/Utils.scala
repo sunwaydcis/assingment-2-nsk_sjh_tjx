f1c0b17cea7dfa1ad586763329328290e9020fc4
NG SHI KEE <23062797@imail.sunway.edu.my>
2025-11-30 22:53:55 +0800

    Update Utils.scala

--- src/main/scala/Utils.scala
+++ src/main/scala/Utils.scala
@@ -3,9 +3,14 @@ object Utils {
   def safeToDouble(str: String): Double =
     try str.toDouble
     catch {case _: Throwable => 0.0}
-    
+
   def safeToInt(str: String): Int =
     try str.toInt
     catch {case _: Throwable => 0}
+
+  def topByFrequency[T](values: Iterable[T]): Option[(T, Int)] =
+    values
+      .groupMapReduce(identity)(_ => 1)(_ + _)
+      .maxByOption(_._2)
 
 }
