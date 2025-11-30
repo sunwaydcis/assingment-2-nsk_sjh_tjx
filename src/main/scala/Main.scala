9207f0f2a63b76344e47783948a14ee629d42765
NG SHI KEE <23062797@imail.sunway.edu.my>
2025-11-30 22:26:49 +0800

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,26 @@
 object Main {
 
-  
+  def loadData(filePath: String): List[Map[String, String]] = {
+    
+    val src = Source.fromFile (filePath, "latin1")
+    
+    try{
+      val lines = src.getLines()
+      
+      if (!lines.hasNext) {
+        Nil
+      } else {
+        
+        val headerLine = lines.next()
+        val headers: Array[String] =
+          headerLine.split(",").map(_.trim)
+          
+        lines.toList.map { line =>
+          val cols = line,split(",").map(_.trim)
+          headers.zipAll(cols, ",").toMap
+        }
+      }
+    } 
+  }
 
 }
