216665dff591ea6e76a86eff3c89d2774cb1302d
NG SHI KEE <23062797@imail.sunway.edu.my>
2025-11-30 22:41:16 +0800

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,44 +3,44 @@ import scala.io.Source
 object Main {
 
   def loadData(filePath: String): List[Map[String, String]] = {
-    
+
     val src = Source.fromFile (filePath, "latin1")
-    
+
     try{
       val lines = src.getLines()
-      
+
       if (!lines.hasNext) {
         Nil
       } else {
-        
+
         val headerLine = lines.next()
         val headers: Array[String] =
           headerLine.split(",").map(_.trim)
-          
+
         lines.toList.map { line =>
           val cols = line.split(",").map(_.trim)
-          headers.zipAll(cols, ", ").toMap
+          headers.zipAll(cols, "", "").toMap
         }
       }
     } finally {
       src.close()
     }
   }
-  
+
   def main(args: Array[String]): Unit ={
     val filePath = "data/Hotel_Dataset.csv"
-    
+
     val data = loadData(filePath)
-    
+
     if (data.isEmpty) {
       println("No data found.")
       return
     }
-    
+
     val analyses: List[IndicatorAnalysis] = List(
       new Q1_HighestBookingCountry
     )
-    
+
     analyses.foreach(_.analyze(data))
   }
 
