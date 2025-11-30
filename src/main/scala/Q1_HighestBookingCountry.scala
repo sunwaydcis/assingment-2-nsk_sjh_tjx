c395f7fd26e7f436edb8770783daa5d03936ea9f
NG SHI KEE <23062797@imail.sunway.edu.my>
2025-11-30 23:30:46 +0800

    Update Q1_HighestBookingCountry.scala

--- src/main/scala/Q1_HighestBookingCountry.scala
+++ src/main/scala/Q1_HighestBookingCountry.scala
@@ -1,29 +1,29 @@
 import Utils._
 import IndicatorAnalysis.Row
 
-class Q1_HighestBookingCountry extends IndicatorAnalysis{
+class Q1_HighestBookingCountry extends IndicatorAnalysis {
 
   private val DestinationKey = "Destination Country"
 
   override def analyze(data: List[Row]): Unit = {
 
     val destinationCountries =
-
+      
       data.view
         .flatMap(_.get(DestinationKey))
         .filter(_.nonEmpty)
 
     val topCountryOpt = topByFrequency(destinationCountries)
 
-    println("[Country Booking Analysis]")
-
     topCountryOpt match {
       case Some((country, count)) =>
-        println(s"Country: $country")
-        println(s"Number of bookings: $count")
-
+        printResult(country, count)
       case None =>
-        println("No valid booking data found.")
+        println("┌─────────────────────────────────────────┐")
+        println("│        COUNTRY BOOKING ANALYSIS         │")
+        println("├─────────────────────────────────────────┤")
+        println("│      No valid booking data found:(      │")
+        println("└─────────────────────────────────────────┘")
     }
   }
 
