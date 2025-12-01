6ea1f3e80222537a15c069c669c715f48a0aa658
Jiaxin0503 <23080880@imail.sunway.edu.my>
2025-12-01 20:08:24 +0800

    Update Q3_MostProfitableHotel.scala

--- src/main/scala/Q3_MostProfitableHotel.scala
+++ src/main/scala/Q3_MostProfitableHotel.scala
@@ -4,14 +4,23 @@ import IndicatorAnalysis.Row
 class Q3_MostProfitableHotel extends IndicatorAnalysis {
 
   private val HotelNameKey = "Hotel Name"
-  private val VisitorsKey = "Number of People"
+  private val VisitorsKey = "No. Of People"
   private val ProfitMarginKey  = "Profit Margin"
 
   override def analyze(data: List[Row]): Unit = {
 
     val validRows = data.filter { row =>
         row.getOrElse(HotelNameKey, "").nonEmpty &&
-        row.getOrElse(VisitorsKey, "").nonEmpty 
+        row.getOrElse(VisitorsKey, "").nonEmpty &&
+        row.getOrElse(ProfitMarginKey, "").nonEmpty
     }
+
+    val groupedByHotel = validRows.groupBy((HotelNameKey))
+
+    val hotelScores: Map[String, Double] =
+      groupedByHotel.view.mapValues { rows =>
+        val scores: List[Double] = rows.flatMap { row =>
+          val visitorsStr = row.getOrElse(VisitorsKey, "0")
+          val profitMarginStr = row.getOrElse(ProfitMarginKey, "0")
   }
 }
