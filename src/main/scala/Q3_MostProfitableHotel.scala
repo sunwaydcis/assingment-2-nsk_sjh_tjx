420dbb3e2d81388d918d926aa222aa2d5fdce3b7
Jiaxin0503 <23080880@imail.sunway.edu.my>
2025-12-01 20:25:38 +0800

    Update Q3_MostProfitableHotel.scala

--- src/main/scala/Q3_MostProfitableHotel.scala
+++ src/main/scala/Q3_MostProfitableHotel.scala
@@ -5,37 +5,46 @@ class Q3_MostProfitableHotel extends IndicatorAnalysis {
 
   private val HotelNameKey = "Hotel Name"
   private val VisitorsKey = "No. Of People"
-  private val ProfitMarginKey  = "Profit Margin"
+  private val ProfitMarginKey = "Profit Margin"
 
   override def analyze(data: List[Row]): Unit = {
 
     val validRows = data.filter { row =>
-        row.getOrElse(HotelNameKey, "").nonEmpty &&
+      row.getOrElse(HotelNameKey, "").nonEmpty &&
         row.getOrElse(VisitorsKey, "").nonEmpty &&
-        row.getOrElse(ProfitMarginKey, "").nonEmpty &&
+        row.getOrElse(ProfitMarginKey, "").nonEmpty
     }
 
     if (validRows.isEmpty) {
       printNoData()
       return
     }
-    
+
     val hotelScores: Map[String, Double] =
       groupedByHotel.view.mapValues { rows =>
         val scores: List[Double] = rows.flatMap { row =>
           val visitorsStr = row.getOrElse(VisitorsKey, "0")
           val profitMarginStr = row.getOrElse(ProfitMarginKey, "0")
-          val visitors     = safeToInt(visitorsStr)
+          val visitors = safeToInt(visitorsStr)
           val profitMargin = safeToDouble(profitMarginStr)
 
           if (visitors <= 0 || profitMargin.isNaN) None
           else Some(visitors.toDouble * profitMargin)
-  }
+        }
+        
         if (scores.isEmpty) Double.NegativeInfinity
         else scores.sum
-        
-        
-        
-        
-        
+      }.toMap
+
+    val bestHotelOpt: Option[(String, Double)] =
+      hotelScores.maxByOption(_._2)
+
+    bestHotelOpt match {
+      case Some((hotel, score))
+        printResult
+      case None 
+        printNoData
+    }
+
+  }
 }
