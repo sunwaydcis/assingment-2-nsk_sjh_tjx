92fd6bdacc8e35c287bad52dbf6c7a0b76644540
Jiaxin0503 <23080880@imail.sunway.edu.my>
2025-12-01 19:55:13 +0800

    Update Q3_MostProfitableHotel.scala

--- src/main/scala/Q3_MostProfitableHotel.scala
+++ src/main/scala/Q3_MostProfitableHotel.scala
@@ -1,3 +1,17 @@
-class Q3_MostProfitableHotel {
+import Utils._
+import IndicatorAnalysis.Row
 
+class Q3_MostProfitableHotel extends IndicatorAnalysis {
+
+  private val HotelNameKey = "Hotel Name"
+  private val VisitorsKey = "Number of People"
+  private val ProfitMarginKey  = "Profit Margin"
+
+  override def analyze(data: List[Row]): Unit = {
+
+    val validRows = data.filter { row =>
+        row.getOrElse(HotelNameKey, "").nonEmpty &&
+        row.getOrElse(VisitorsKey, "").nonEmpty 
+    }
+  }
 }
